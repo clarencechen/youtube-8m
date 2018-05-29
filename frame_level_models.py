fe5677d3f2d15d0173c367257db0aa4254fb7d3b
clarencechen <clarencechenct@gmail.com>
2018-05-29 14:54:06 -0700

    typo

--- frame_level_models.py
+++ frame_level_models.py
@@ -268,14 +268,14 @@ class TCNModel(models.BaseModel):
       pad1 = tf.pad(inputs, pad_tensor, name='pad1')
       conv1 = layers.conv2d(pad1, out_channels, kernel_size, data_format='NWC', stride=1, padding='VALID', rate=dilation, 
         normalizer_fn=layers.batch_norm, normalizer_params=bn_params, scope='conv1')
-      dropout1 = layers.dropout(conv1[:, :, :-(kernel_size -1)*dilation], 
+      dropout1 = layers.dropout(conv1[:, :-(kernel_size -1)*dilation, :], 
         keep_prob=keep_prob, is_training=is_training, scope='dropout1')
       print(dropout1.shape)
       
       pad2 = tf.pad(dropout1, pad_tensor, name='pad2')
       conv2 = layers.conv2d(pad2, out_channels, kernel_size, data_format='NWC', stride=1, padding='VALID', rate=dilation, 
         normalizer_fn=layers.batch_norm, normalizer_params=bn_params, scope='conv2')
-      dropout2 = layers.dropout(conv2[:, :, :-(kernel_size -1)*dilation], 
+      dropout2 = layers.dropout(conv2[:, :-(kernel_size -1)*dilation, :], 
         keep_prob=keep_prob, is_training=is_training, scope='dropout2')
       print(dropout2.shape)
 
