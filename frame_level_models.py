0c3cbd45d9db96c97fe144fa13149ae937bff4cb
clarencechen <clarencechenct@gmail.com>
2018-05-29 16:50:58 -0700

    add fc

--- frame_level_models.py
+++ frame_level_models.py
@@ -280,7 +280,7 @@ class TCNModel(models.BaseModel):
 
     tcn_params = [[hidden_size*(2 ** (i // 2)), kernel_size, 2 ** i] for i in range(number_of_layers)]
     tcn_out = layers.stack(model_input, TCNBlock, tcn_params)
-    tcn_pooled = tf.layers.average_pooling1d(tcn_out, pool_size=inputs.shape[-2], strides=1, name='tcn_pool')
+    tcn_pooled = tf.layers.average_pooling1d(tcn_out, pool_size=model_input.shape[-2], strides=1, name='tcn_pool')
     fc_out = layers.fully_connected(tf.squeeze(tcn_pooled), vocab_size, tf.sigmoid, batch_norm, bn_params)
     aggregated_model = getattr(video_level_models,
                                FLAGS.video_level_classifier_model)
