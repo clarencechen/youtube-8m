7c324d984280a4b7d2e37c5df9bcdf0b07b2cecb
clarencechen <clarencechenct@gmail.com>
2018-05-29 21:05:33 -0700

    rmsprop

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
   # Other flags.
   flags.DEFINE_integer("num_readers", 8,
                        "How many threads to use for reading input files.")
-  flags.DEFINE_string("optimizer", "NadamOptimizer",
+  flags.DEFINE_string("optimizer", "RMSPropOptimizer",
                       "What optimizer class to use.")
   flags.DEFINE_float("clip_gradient_norm", 1.0, "Norm to clip gradients to.")
   flags.DEFINE_bool(
