05fd5db75fd55e793aff515b2051ca8ca5a12e14
clarencechen <clarencechenct@gmail.com>
2018-06-01 12:38:02 -0700

    faster decay

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
   flags.DEFINE_float("learning_rate_decay_examples", 10000,
                      "Multiply current learning rate by learning_rate_decay "
                      "every learning_rate_decay_examples.")
-  flags.DEFINE_integer("num_epochs", 5,
+  flags.DEFINE_integer("num_epochs", 1,
                        "How many passes to make over the dataset before "
                        "halting training.")
   flags.DEFINE_integer("max_steps", None,
